613b9a031396f9b950ecdfa778f28c979d2f26cf
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-04 16:07:03 -0600

    Showing each diamond on the screen

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -1,3 +1,5 @@
+var AMOUNT_DIAMONDS = 30; //Variable Global
+
 GamePlayManager = {   //ObjetoGamePlayManager
     init: function(){
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL; //Haciendo nuestro juego totalmente responsivo
@@ -8,20 +10,29 @@ GamePlayManager = {   //ObjetoGamePlayManager
     },
     preload: function(){
         game.load.image('background', 'assets/images/background.png'); //cargando la imagen del backgroud
-        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2) //Cargando nuestro spritesheet en el cual tomamos el primer cballo de mar indicamos las dimensiones del ancho y el largo en conjunto con cuantas imagenes contiene la imagen principal
+        game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2); //Cargando nuestro spritesheet en el cual tomamos el primer cballo de mar indicamos las dimensiones del ancho y el largo en conjunto con cuantas imagenes contiene la imagen principal
+        game.load.spritesheet('diamonds', 'assets/images/diamonds.png', 81, 84, 4);
     },
     create: function(){
-        console.log("Create");
         game.add.sprite(0,0,'background'); // implementando el background en la esquina superior izquierda coordenadas 0,0
         this.horse = game.add.sprite(0,0, 'horse'); //Guardando la instancia de nuestro caballo para poder acceder a sus propiedades, 'this' se ocupa para que este se encuentre dentro de todo nuestro objeto GamePlayManager
-        this.horse.frame = 1 //Accediendo a la propiedad frame y toma el dibujo con los ojos abiertos por el 1, si ponemos 0 toma el dibujo con ojos cerrados.
-        this.horse.x = game.width/2  //Posicionando nuestro caballo en el centro de la pantalla
-        this.horse.y = game.height/2 //Posicionando nuestro caballo en el centro de la pantalla
+        this.horse.frame = 1; //Accediendo a la propiedad frame y toma el dibujo con los ojos abiertos por el 1, si ponemos 0 toma el dibujo con ojos cerrados.
+        this.horse.x = game.width/2;  //Posicionando nuestro caballo en el centro de la pantalla
+        this.horse.y = game.height/2; //Posicionando nuestro caballo en el centro de la pantalla
         this.horse.anchor.setTo(0.5); //Tomando el anchor de nuestro caballo justamente en su centro
 
         game.input.onDown.add(this.onTap, this); // Funcion para cuando el usuario de click en nuestro juego nuestro flag sea verdadero
-    },
 
+        this.diamonds = [];
+        for(var i=0; i<AMOUNT_DIAMONDS; i++){ //creando la cantidad de diamantes que  la variable global señala
+            var diamond = game.add.sprite(100,100 ,'diamonds'); //creando el diamante
+            diamond.frame = game.rnd.integerInRange(0,3);  //tomando un random para escoger 1 de los posibles 4 diamantes
+            diamond.scale.setTo( 0.30 + game.rnd.frac()); //Asignando un tamaño random al diamante
+            diamond.anchor.setTo(0.5); // Asignando el anchor en el centro
+            diamond.x = game.rnd.integerInRange(50,1050); //Escogiendo una posicion al azar para x entre 50 y 1050
+            diamond.y = game.rnd.integerInRange(50,600);  //Escogiendo una posicion al azar para x entre 50 y 600
+        }
+    },
     onTap:function(){
         this.flagFirstMouseDown = true; //vuelve nuestro flag en verdadero
     },
