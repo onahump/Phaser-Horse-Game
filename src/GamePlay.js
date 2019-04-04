e94673cdec97e793dbab6419f214a3ce80f9332c
OKYSoft Developer <developer@MacBook-Pro-de-OKYSoft-4.local>
2019-04-04 13:44:12 -0600

    Runnig on canvas way

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -15,7 +15,7 @@ GamePlayManager = {   //ObjetoGamePlayManager
 }
 
 
-var game = new Phaser.Game(1136,640, Phaser.AUTO); //instanciamos juego en facer con sus especificaciones
+var game = new Phaser.Game(1136,640, Phaser.CANVAS); //instanciamos juego en facer con sus especificaciones
 
 game.state.add('gameplay',GamePlayManager); //Agregamos un estado que le vamos a llamar gameplay y se le asigna el objeto GamePlayMañager
 game.state.start('gameplay'); //Llama a cada uno de los metodos
